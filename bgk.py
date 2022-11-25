dcc2f275b1fc4ba5c3545f1de71156a4e77c74e0
Anthony Chang <anthony.chang@utexas.edu>
2022-11-25 10:15:35 -0600

    initial file

---
+++ bgk.py
@@ -0,0 +1,14 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+
+
+def rhs_bgk(t, y):
+    pass
+
+
+# Input variables.
+numCx = 100
+numCy = 100
+numCz = 100
+numXj = 10  # number of spatial points
+
